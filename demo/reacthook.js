a66da354e3661d97fe770826382e37e6f7cf2c3c
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-03-28 05:47:37 -0700

    added classy theme

--- demo/reacthook.js
+++ demo/reacthook.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 function Example() {
-  const [count, setCount] = useState(0);
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
